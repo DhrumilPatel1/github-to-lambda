e825a207bb693aa5cc50557751d40833bd532d2a
arcus_nodejs_dev <abhishek.patel@arcuspartner.com>
2022-06-07 12:33:53 +0530

    merge

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 module.exports.handler =async function handler() {
     console.log("I asasssssssadasdddaaM  Dhrsadfgdsasumil Patel");
 
-    return {a:"a by lambda"}
+    return {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": JSON.stringify({a:"a",b:"b"}),
+        "isBase64Encoded": false
+    };
 }
