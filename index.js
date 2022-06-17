22de1b9406ad867449bfcc7f6f7704ea99e6a869
arcus_nodejs_dev <abhishek.patel@arcuspartner.com>
2022-06-17 11:27:39 +0530

    merge

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports.handler =async function handler() {
-    console.log("I asasssssssadasdddaaM  Dhrsadfgdsasumil Patel");
+    console.log("I asassssssddddsadasdddaaM  Dhrsadfgdsasumil Patel");
 
     return {
         "statusCode": 200,
         "headers": {
             "my_header": "my_value"
         },
-        "body": JSON.stringify({a:"a",b:"b"}),
+        "body": JSON.stringify(responseBody),
         "isBase64Encoded": false
     };
 }
